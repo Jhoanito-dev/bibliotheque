9b95e927bb1fad935e0ba9ecfab2031e033d3621
Jhoanito <jhoanitogilbert@gmail.com>
2025-07-03 19:18:59 +0100

    Ajout des repositories, API de base et configuration de sécurité pour l'Etape 2

---
+++ src/main/java/com/example/bibliotheque/repository/JourFerieRepository.java
@@ -0,0 +1,7 @@
+package com.example.bibliotheque.repository;
+
+import com.example.bibliotheque.model.JourFerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JourFerieRepository extends JpaRepository<JourFerie, Long> {
+}
